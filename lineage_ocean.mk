fc8b3bb82a8fa9f84eb167d5449408a184c03d90
Rondeau79 <rondeau7@gmail.com>
2020-03-31 18:34:25 -0400

    Update lineage_ocean.mk

--- lineage_ocean.mk
+++ lineage_ocean.mk
@@ -35,5 +35,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=ocean \
 	PRIVATE_BUILD_DESC="ocean-user 9 PPO29.114-134 fee207 release-keys"
 
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := motorola/ocean/ocean:9/PPO29.114-134/fee207:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint="google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys"
