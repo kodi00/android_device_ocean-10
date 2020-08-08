b6ccb023bb1b15836536add3f29db8ef4a9ed53e
kodi00 <69102217+kodi00@users.noreply.github.com>
2020-08-08 20:16:58 +0000

    Update descendant_ocean.mk

--- descendant_ocean.mk
+++ descendant_ocean.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+#Gapps
+TARGET_GAPPS_ARCH := arm64
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
